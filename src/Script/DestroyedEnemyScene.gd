30c83f2db91e982b984eb7124b13217573d15888
Szymon Świędrych <szymek_sw@outlook.com>
2022-12-17 18:19:38 +0100

    Tweak particles

--- src/Script/DestroyedEnemyScene.gd
+++ src/Script/DestroyedEnemyScene.gd
@@ -2,10 +2,13 @@ extends Node3D
 
 
 func _ready():
-	$Particles1.emitting = true
-	$Particles2.emitting = true
 	$SFX.play()
 
 
 func _on_Timer_timeout():
-	queue_free()
+	pass
+
+
+func _on_play_timer_timeout():
+	$Particles1.emitting = true
+	$Particles2.emitting = true
