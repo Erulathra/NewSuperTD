e5a49dd5eef9e342107d06ce59c52b1c5dcf02b4
Szymon Świędrych <szymek_sw@outlook.com>
2023-01-20 18:27:29 +0100

    Add get_neighbours

--- Tiles/Tile.gd
+++ Tiles/Tile.gd
@@ -23,12 +23,33 @@ func _process(_delta):
 
 
 func _on_area_3d_mouse_entered():
-	material.albedo_color = normal_color.blend(hover_color)
+	for neighbor in get_neighbors():
+		if neighbor is Tile:
+			neighbor.material.albedo_color = normal_color.blend(hover_color)
+
 
 
 func _on_area_3d_mouse_exited():
-	material.albedo_color = normal_color
+	for neighbor in get_neighbors():
+		if neighbor is Tile:
+			neighbor.material.albedo_color = normal_color
 
 
-func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+func _on_area_3d_input_event(_camera, _event, _position, _normal, _shape_idx):
+	if _event is InputEventMouseButton:
+		print_debug("HEllo")
 	pass # Replace with function body.
+
+
+func get_neighbors():
+	var result = []
+
+	var global_basis = global_transform.basis;
+	for vector in [global_basis.x, -global_basis.x, global_basis.z, -global_basis.z]:
+		var raycast_query = PhysicsRayQueryParameters3D.create(position, position + vector * 0.5)
+		raycast_query.collide_with_areas = true
+		var raycast_result = get_world_3d().direct_space_state.intersect_ray(raycast_query)
+		if not raycast_result.is_empty():
+			result.append(raycast_result.collider.get_node("../../"))
+		
+	return result;
